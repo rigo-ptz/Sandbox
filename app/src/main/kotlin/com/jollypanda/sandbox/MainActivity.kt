51b282719b60d9838069a6227c1b95977e5a6e87
Yamushev Igor <iy@trinitydigital.ru>
2018-03-30 15:36:23 +0300

    Check some rx

--- app/src/main/kotlin/com/jollypanda/sandbox/MainActivity.kt
+++ app/src/main/kotlin/com/jollypanda/sandbox/MainActivity.kt
@@ -2,7 +2,9 @@ package com.jollypanda.sandbox
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.jollypanda.sandbox.chat_containers.ChatActivity
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
@@ -15,5 +17,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnChat.setOnClickListener { startActivity(ChatActivity.getStartIntent(this)) }
+        checkRx()
+    }
+    
+    private fun checkRx() {
+        val array = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        Observable.just(array)
+            .flatMapIterable { it }
+            .buffer(20)
+            .subscribe {
+                Log.e("CHUNK", it.toString())
+            }
     }
 }
